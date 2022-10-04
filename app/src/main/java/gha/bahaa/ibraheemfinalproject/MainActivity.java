c02d0f0faddf2f0c2b6ff349388e2680511ef496
Owner <noran@danon.tzafonet.org.il>
2022-10-04 08:53:45 +0300

    mmm vhyuu

--- app/src/main/java/gha/bahaa/ibraheemfinalproject/MainActivity.java
+++ app/src/main/java/gha/bahaa/ibraheemfinalproject/MainActivity.java
@@ -1,7 +1,12 @@
 package gha.bahaa.ibraheemfinalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    /**
+     * لبناء واضافه قائمه
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.Settings)
+        {
+            Intent i=new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(i);
+
+        }
+        return true;
     }
 }
---
+++ app/src/main/java/gha/bahaa/ibraheemfinalproject/SettingsActivity.java
@@ -0,0 +1,13 @@
+package gha.bahaa.ibraheemfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
